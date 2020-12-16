b6b191cba22c332596ab4e930fae27410c8b5e81
Stop565 <55998673+Stop565@users.noreply.github.com>
2020-12-16 14:57:24 +0200

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,11 @@
+# включить  " Calculator.h "
+
+int  Calculator :: Add ( двойное a, двойное b)
+{
+	вернуть a + b + 0,5 ;
+}
+
+int  Calculator :: Sub ( двойное a, двойное b)
+{
+	return  Add (a, -b);
+}
