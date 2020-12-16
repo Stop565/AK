19b78d3b6203d1a725df1cc9cf6f625fc07912d6
Stop565 <55998673+Stop565@users.noreply.github.com>
2020-12-16 14:56:34 +0200

    Create Calculator.h

---
+++ Calculator.h
@@ -0,0 +1,11 @@
+# ifndef CALCULATOR_H
+# определить  CALCULATOR_H
+
+ калькулятор классов
+{
+	общественность:
+		int  Add ( двойной , двойной );
+		int  Sub ( двойной , двойной );
+};
+
+# endif // CALCULATOR_H
